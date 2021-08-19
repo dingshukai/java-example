b710c2b780ef14b69c81d8cd9142dc2e1bd29519
dingshukai <dingshukai@meituan.com>
2021-08-19 21:00:07 +0800

    双端数组

---
+++ src/main/java/array/CircularArray.java
@@ -0,0 +1,37 @@
+package array;
+
+/**
+ * 圆形数组，把数组看成是一个圆环。
+ *
+ * @author: dingshukai
+ * @date: 2021/8/19
+ */
+public class CircularArray {
+
+    public int size() {
+        return 0;
+    }
+
+    //获得第n个元素
+    public int get(int index) {
+        return 0;
+    }
+
+    // 在头部添加元素。
+    // 不挪动数组的其他元素（下同），而是在第一个元素的左边添加。如果左边没有位置了，则折行添加到数组的最后一个位置。如果最后一个位置也满了，则扩容。
+    public void addFirst(int elem) {
+
+    }
+
+    // 删除第一个元素。
+    //
+    public int removeFirst() {
+        return 0;
+    }
+
+    public int getFirst() {return 0;}
+
+    public void addLast(int elem) {}
+    public int removeLast() {return 0;}
+    public int getLast() {return 0;}
+ }
