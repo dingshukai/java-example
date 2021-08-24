b98099cc8e53bb0572f5180ad30824c814aa0570
dingshukai <dingshukai@meituan.com>
2021-08-24 11:17:20 +0800

    双端数组，一部分代码

--- src/main/java/array/CircularArray.java
+++ src/main/java/array/CircularArray.java
@@ -8,25 +8,55 @@ package array;
  */
 public class CircularArray {
 
+    private int[] data;
+    private static int INIT_CAPACITY = 8;
+    private int size;
+    private int capacity;
+    private int head;
+    private int tail;
+
+    public CircularArray() {
+        size = 0;
+        capacity = INIT_CAPACITY;
+        data = new int[capacity];
+    }
+
     public int size() {
-        return 0;
+        return size;
     }
 
     //获得第n个元素
     public int get(int index) {
-        return 0;
+        return data[(index+head)%capacity];
     }
 
     // 在头部添加元素。
     // 不挪动数组的其他元素（下同），而是在第一个元素的左边添加。如果左边没有位置了，则折行添加到数组的最后一个位置。如果最后一个位置也满了，则扩容。
     public void addFirst(int elem) {
+        int beforeHead = indexBeforeHead();
+        if(beforeHead==tail) {
+            grow();
+            beforeHead = indexBeforeHead();
+        }
 
+        data[beforeHead] = elem;
+        size++;
+        head = beforeHead;
+    }
+
+    private int indexBeforeHead() {
+        return (head -1 + capacity) % capacity;
     }
 
     // 删除第一个元素。
     //
     public int removeFirst() {
-        return 0;
+        if(size<=0)
+            throw new ArrayIndexOutOfBoundsException("data is empty");
+        int ret = data[head];
+        head = (head+1)%capacity;
+        size --;
+        return ret;
     }
 
     public int getFirst() {return 0;}
