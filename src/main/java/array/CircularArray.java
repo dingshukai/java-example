6081b305f14eaca22f23a737bbe773578a533573
dingshukai <dingshukai@meituan.com>
2021-08-25 11:33:11 +0800

    双端数组，双向队列

--- src/main/java/array/CircularArray.java
+++ src/main/java/array/CircularArray.java
@@ -33,20 +33,30 @@ public class CircularArray {
     // 在头部添加元素。
     // 不挪动数组的其他元素（下同），而是在第一个元素的左边添加。如果左边没有位置了，则折行添加到数组的最后一个位置。如果最后一个位置也满了，则扩容。
     public void addFirst(int elem) {
-        int beforeHead = indexBeforeHead();
-        if(beforeHead==tail) {
+        if(size==capacity)
             grow();
-            beforeHead = indexBeforeHead();
-        }
-
-        data[beforeHead] = elem;
+        head = (head-1+capacity)%capacity;
+        data[head] = elem;
         size++;
-        head = beforeHead;
     }
 
-    private int indexBeforeHead() {
-        return (head -1 + capacity) % capacity;
+    private void grow() {
+        int[] newArray = new int[data.length*2];
+        if(head<=tail) {
+            for(int i=head; i<=tail; i++)
+                newArray[i] = data[i];
+        }else {
+            for(int i=0; i<=tail; i++)
+                newArray[i] = data[i];
+            int more = newArray.length - data.length;
+            for(int i=head; i<=data.length; i++)
+                newArray[more+i] = data[i];
+            head = more + head;
+        }
+        data = newArray;
+        capacity = data.length;
     }
+
 
     // 删除第一个元素。
     //
@@ -59,9 +69,30 @@ public class CircularArray {
         return ret;
     }
 
-    public int getFirst() {return 0;}
+    public int getFirst() {
+        if(size==0)
+            throw new IndexOutOfBoundsException();
+        return data[head];
+    }
 
-    public void addLast(int elem) {}
-    public int removeLast() {return 0;}
-    public int getLast() {return 0;}
+    public void addLast(int elem) {
+        if(size==capacity)
+            grow();
+        tail = (tail + 1)%capacity;
+        size++;
+        data[tail] = elem;
+    }
+    public int removeLast() {
+        if(size<=0)
+            throw new ArrayIndexOutOfBoundsException("data is empty");
+        int ret = data[tail];
+        tail = (tail -1 +capacity)%capacity;
+        size --;
+        return ret;
+    }
+    public int getLast() {
+        if(size<=0)
+            throw new ArrayIndexOutOfBoundsException("data is empty");
+        return data[tail];
+    }
  }
--- src/main/java/linkedlist/IntQueue.java
+++ src/main/java/linkedlist/IntQueue.java
@@ -10,28 +10,85 @@ package linkedlist;
  * @date: 2021/8/17
  */
 public class IntQueue {
+    private Node head;
+    private Node tail;
+    private int size ;
+
+    public IntQueue() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
 
     // 获得头部元素
-    public int getFirst( ) {return 0;}
+    public int getFirst( ) {
+        if(head==null)
+            throw new IndexOutOfBoundsException();
+        return head.data;
+    }
     // 在头部添加元素
-    public void addFirst(int elem) {}
+    public void addFirst(int elem) {
+        Node n = new Node();
+        n.next = head;
+        head = n;
+        n.data = elem;
+        size++;
+        if(size==1)
+            tail = n;
+    }
 
     // 删除并返回头部元素
-    public int removeFirst() {return 0;}
+    public int removeFirst() {
+        if(head==null)
+            throw new IndexOutOfBoundsException();
+        Node rem = head;
+        head = rem.next;
+        if(head==null)
+            tail = null;
+        size--;
+        return rem.data;
+    }
 
     // 获得尾部元素
-    public int getLast( ) {return 0;}
+    public int getLast( ) {
+        if(tail==null)
+            throw new IndexOutOfBoundsException();
+        return tail.data;
+    }
 
     // 在尾部添加元素
-    public void addLast(int elem) {}
+    public void addLast(int elem) {
+        Node n = new Node();
+        n.data = elem;
+        n.prev = tail;
+        tail = n;
+        if(head==null)
+            head = tail;
+        size ++;
+    }
 
     // 删除并返回尾部元素
-    public int removeLast() {return 0;}
+    public int removeLast() {
+        if(tail==null)
+            throw new IndexOutOfBoundsException();
+        Node last = tail;
+        tail = tail.prev;
+        if(tail==null)
+            head = null;
+        size--;
+        return last.data;
+    }
 
     // 队列大小
-    int size() {return 0;}
+    int size() {return size;}
 
     // 序列化
     @Override
     public String toString() {return null;}
+
+    private class Node {
+        int data;
+        Node prev;
+        Node next;
+    }
 }
